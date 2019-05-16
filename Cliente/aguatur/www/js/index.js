4d76fc28b6dc71f29e6277ea0b32778d45ccaa1e
Estudiante Bloque 09 Sala 208  B09S208est <B09S208est@AULASMED.UPB>
2019-05-16 14:46:19 -0500

    configuración de login

--- Cliente/aguatur/www/js/index.js
+++ Cliente/aguatur/www/js/index.js
@@ -26,6 +26,7 @@ var app = {
         document.getElementById("returnL").addEventListener("click",()=>{app.changeView(login,panelRegisterLogin)});
         document.getElementById("initNotRegisteredR").addEventListener("click",()=>{app.changeView(register,home)});
         document.getElementById("cancelRegister").addEventListener("click",()=>{app.changeView(register,panelRegisterLogin)});
+        document.getElementById("cancelRegister").addEventListener("click",()=>{app.changeView(register,panelRegisterLogin)});
 
         $('#makeRegister').attr('disabled','disabled');
     },
@@ -59,7 +60,8 @@ $('#formLoginData').submit(function (e) {
     data.forEach((item)=>{
         arrayData[item.name] = item.value;
     });
-    dataLogin = arrayData;
+    console.log(arrayData["email"]+arrayData["password"]);
+    findAccount(arrayData["email"],arrayData["password"]);
 });
 
 $('#registerData').submit(function (e) {
